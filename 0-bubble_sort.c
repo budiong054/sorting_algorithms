06fc65349580df43ec90a051be06d54f37e947e3
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-11-05 09:49:57 +0000

    Add bubble sort, header file and gitignore file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using
+ * the Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t j;
+	int swap;
+	size_t n = size;
+	int temp;
+
+	while (1)
+	{
+		swap = 0; /* false */
+		for (j = 1; j < n; j++)
+		{
+			if (array[j - 1] > array[j])
+			{
+				temp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = temp;
+				swap = 1; /* true */
+				print_array(array, size);
+			}
+		}
+		
+		if (!swap)
+			break;
+		n--;
+	}
+}
